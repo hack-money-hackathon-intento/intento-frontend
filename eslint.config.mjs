2268aeeae28db908219d5974da218751a0f07cb2
salviega <salviega6@gmail.com>
2026-02-01 08:53:19 -0500

    🔧 Added eslint config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,59 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+// Recomendado por Next si usas Prettier:
+import prettier from 'eslint-config-prettier/flat'
+import reactHooks from 'eslint-plugin-react-hooks'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
+import globals from 'globals'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default defineConfig([
+	...nextVitals,
+	...nextTs,
+	prettier,
 
-export default eslintConfig;
+	globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+
+	{
+		files: ['**/*.{js,jsx,mjs,ts,tsx}'],
+
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: {
+				...globals.browser
+			}
+		},
+
+		plugins: {
+			'react-hooks': reactHooks,
+			'simple-import-sort': simpleImportSort,
+			'unused-imports': unusedImports
+		},
+
+		rules: {
+			eqeqeq: ['warn', 'always'],
+
+			'@typescript-eslint/no-unused-vars': 'off',
+
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'warn',
+
+			'unused-imports/no-unused-imports': 'warn',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_'
+				}
+			],
+
+			'react-hooks/rules-of-hooks': 'error',
+			'react-hooks/exhaustive-deps': 'warn'
+		}
+	}
+])
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**'
+			}
+		]
+	}
+}
+export default nextConfig
