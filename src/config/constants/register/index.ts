ac90d2110c4e94473ca3161661ceeaaac0351775
salviega <salviega6@gmail.com>
2026-02-02 17:17:13 -0500

    🎨 By Standar

--- src/config/constants/register/index.ts
+++ src/config/constants/register/index.ts
@@ -1,10 +1,16 @@
-import { ensureEnvVar } from "@/helpers/ensure-env.helper"
+import { ensureEnvVar } from '@/helpers/ensure-env.helper'
 
 export const register = {
-  octav: {
-    apiKey: ensureEnvVar(process.env.NEXT_PUBLIC_OCTAV_API_KEY, 'NEXT_PUBLIC_OCTAV_API_KEY')
-  },
-  oneInch: {
-    apiKey: ensureEnvVar(process.env.NEXT_PUBLIC_ONE_INCH_API_KEY, 'NEXT_PUBLIC_ONE_INCH_API_KEY')
-  }
+	octav: {
+		apiKey: ensureEnvVar(
+			process.env.NEXT_PUBLIC_OCTAV_API_KEY,
+			'NEXT_PUBLIC_OCTAV_API_KEY'
+		)
+	},
+	oneInch: {
+		apiKey: ensureEnvVar(
+			process.env.NEXT_PUBLIC_ONE_INCH_API_KEY,
+			'NEXT_PUBLIC_ONE_INCH_API_KEY'
+		)
+	}
 }
